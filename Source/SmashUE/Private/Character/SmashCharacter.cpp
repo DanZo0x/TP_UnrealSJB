ff56e4622619406218d2a9b037f795bd78f71b5f
Dan <dan.cheype@gmail.com>
2024-11-06 22:06:16 +0100

    [ADD] Player spawn rotation

--- Source/SmashUE/Private/Character/SmashCharacter.cpp
+++ Source/SmashUE/Private/Character/SmashCharacter.cpp
@@ -3,32 +3,44 @@
 
 #include "Character/SmashCharacter.h"
 
-// Sets default values
 ASmashCharacter::ASmashCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 }
 
-// Called when the game starts or when spawned
 void ASmashCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
-// Called every frame
 void ASmashCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	RotateMeshUsingOrientX();
 }
 
-// Called to bind functionality to input
 void ASmashCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+}
+
+float ASmashCharacter::GetOrient() const
+{
+	return OrientX;
+}
+
+void ASmashCharacter::SetOrientX(float NewOrientX)
+{
+	OrientX = NewOrientX;
+}
+
+void ASmashCharacter::RotateMeshUsingOrientX() const
+{
+	FRotator Rotation = GetMesh()->GetRelativeRotation();
+	Rotation.Yaw = -90.0f * OrientX;
+	GetMesh()->SetRelativeRotation(Rotation);
 }
 
--- Source/SmashUE/Public/Character/SmashCharacter.h
+++ Source/SmashUE/Public/Character/SmashCharacter.h
@@ -11,19 +11,31 @@ class SMASHUE_API ASmashCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
+#pragma region Unreal Default
 public:
-	// Sets default values for this character's properties
 	ASmashCharacter();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+#pragma endregion
+
+#pragma region Orient
+
+public:
+	float GetOrient() const;
+
+	void SetOrientX(float NewOrientX);
+
+protected:
+	UPROPERTY(BlueprintReadOnly)
+	float OrientX = 1.0f;
+
+	void RotateMeshUsingOrientX() const;
+#pragma endregion
 };
