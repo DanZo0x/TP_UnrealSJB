099b889dc4458c7ed0d88a56468dc6b9ab7359ca
Dan <dan.cheype@gmail.com>
2024-11-06 21:50:35 +0100

    [ADD] Spawn players

--- Source/SmashUE/Private/Match/MatchGameMode.cpp
+++ Source/SmashUE/Private/Match/MatchGameMode.cpp
@@ -5,7 +5,7 @@
 
 #include "Arena/ArenaPlayerStart.h"
 #include "Kismet/GameplayStatics.h"
-
+#include "Character/SmashCharacter.h"
 
 void AMatchGameMode::BeginPlay()
 {
@@ -13,16 +13,7 @@ void AMatchGameMode::BeginPlay()
 
 	TArray<AArenaPlayerStart*> PlayerStartPoints;
 	FindPlayerStartActorsInArena(PlayerStartPoints);
-
-	for (AArenaPlayerStart* PlayerStartPoint : PlayerStartPoints)
-	{
-		GEngine->AddOnScreenDebugMessage(
-			-1,
-			3.f,
-			FColor::Cyan,
-			PlayerStartPoint->GetFName().ToString()
-			);
-	}
+	SpawnCharacters(PlayerStartPoints);
 }
 
 void AMatchGameMode::FindPlayerStartActorsInArena(TArray<AArenaPlayerStart*>& ResultsActors)
@@ -36,5 +27,47 @@ void AMatchGameMode::FindPlayerStartActorsInArena(TArray<AArenaPlayerStart*>& Re
 		if (ArenaPlayerStartActor == nullptr) continue;
 
 		ResultsActors.Add(ArenaPlayerStartActor);
+	}
+}
+
+void AMatchGameMode::SpawnCharacters(const TArray<AArenaPlayerStart*>& SpawnPoints)
+{
+	for (AArenaPlayerStart* SpawnPoint : SpawnPoints)
+	{
+		EAutoReceiveInput::Type InputType = SpawnPoint->AutoReceiveInput.GetValue();
+		TSubclassOf<ASmashCharacter> SmashCharacterClass = GetSmashCharacterClassFromInputType(InputType);
+		if (SmashCharacterClass == nullptr) continue;
+
+		ASmashCharacter* NewCharacter = GetWorld()->SpawnActorDeferred<ASmashCharacter>(
+			SmashCharacterClass,
+			SpawnPoint->GetTransform()
+		);
+
+		if (NewCharacter == nullptr) continue;
+		NewCharacter->AutoPossessPlayer = SpawnPoint->AutoReceiveInput;
+		NewCharacter->FinishSpawning(SpawnPoint->GetTransform());
+
+		CharactersInsideArena.Add(NewCharacter);
+	}
+}
+
+TSubclassOf<ASmashCharacter> AMatchGameMode::GetSmashCharacterClassFromInputType(EAutoReceiveInput::Type InputType) const
+{
+	switch (InputType)
+	{
+	case EAutoReceiveInput::Player0:
+		return SmashCharacterClassP0;
+
+	case EAutoReceiveInput::Player1:
+		return SmashCharacterClassP1;
+
+	case EAutoReceiveInput::Player2:
+		return SmashCharacterClassP2;
+
+	case EAutoReceiveInput::Player3:
+		return SmashCharacterClassP3;
+
+	default:
+		return nullptr;
 	}
 }
